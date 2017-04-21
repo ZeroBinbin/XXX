03f0a4054f35dd74c74dd62c715b4c80a9fe6507
zeroBinBin <727832784@qq.com>
2017-04-21 17:58:48 +0800

    2017-4-21

--- src/core.js
+++ src/core.js
@@ -1,3 +1,28 @@
 /**
  * Created by Administrator on 2017/4/20 0020.
  */
+var _XXX = window.XXX ;
+
+class XXX {
+    constructor(args){
+
+    }
+    //静态方法
+
+    /**
+     * 无冲突处理方法
+     * @returns {XXX}
+     */
+    static noConflict(){
+        window.XXX = _XXX;
+        return XXX
+    }
+
+    /**
+     * 对象扩展
+     */
+    static extend(){
+
+    }
+}
+export { XXX }
---
+++ src/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2017/4/21 0021.
+ */
+import { XXX }  from './core'
+
+export  { XXX }
